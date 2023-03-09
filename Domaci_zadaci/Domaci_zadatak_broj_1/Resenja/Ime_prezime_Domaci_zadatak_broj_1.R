04878f057016523c98a8bd441224b9e9de157722
BursacPetar <bursac.petar3@gmail.com>
2023-03-09 13:56:25 +0100

    ...

---
+++ Domaci_zadaci/Domaci_zadatak_broj_1/Resenja/Ime_prezime_Domaci_zadatak_broj_1.R
@@ -0,0 +1,41 @@
+# Ime:
+# Prezime:
+# Broj indeksa:
+# Datum:
+# Vežba broj: 
+# ------------------------------------------------------------------------------
+
+# Zadatak 1:
+#           a) Pronadji operator koji za rezultat operacije deljenja daje celobrojni deo rezultat (npr 111 `operator` 100 = 1).
+#           b) Pronadji operator koji za rezultat operacije deljenja daje decimalni deo (npr 111 `operator` 100 = 0.11).
+# ------------------------------------------------------------------------------
+
+
+# Zadatak 2:
+#           a) Pronadjite funkciju koja kreira 2x6 matricu od vrednosti koje se nalaze u promenljvoj v <- c(1,2,3,4,5,6,7,8,9,10,11,12).
+#           b) Kreirati vektor koji ce sadrzati imena studenata u ucionici.
+# ------------------------------------------------------------------------------
+
+
+
+# Zadatak 3:
+#           a) Uraditi transformaciju datih vrednosti temperatura u Fahrenheit-ima (F) u Celsius stepene (C): 45, 96, 451
+#           b) Uraditi transformaciju datih vrednosti temperatura u Celsius stepenima (C) u Fahrenheit-e (F): -65, 100, 20
+#           c) Napisati funckiju za transformaciju stepena C u F, gde je kao ulazni parametar broj
+#           d) Napisati funckiju za transformaciju stepena F u C, gde je kao ulazni parametar broj
+# ------------------------------------------------------------------------------
+
+
+# Zadatak 4:
+#           a) Ivršiti naredni kod i koristiti typeof() i class() funkcije i pogledati kako R tretira svaki novi kreiran vektor?
+# ------------------------------------------------------------------------------
+num_char <- c(1, 2, 3, "a")
+num_logical <- c(1, 2, 3, TRUE)
+char_logical <- c("a", "b", "c", TRUE)
+tricky <- c(1, 2, 3, "4")
+
+
+# Zadatak 5:
+#           a) Napraviti data.frame sa sledećim kolonama: ID, prezime, ime, broj_indeksa, godina_upisa, smer, prgramski_jezik, email, i za iste kolone popuniti odgovarajuće vrednosti.
+#           b) Atributi smer i programski jezik je potrebno da budu fakotorske promenljive.
+# ------------------------------------------------------------------------------
